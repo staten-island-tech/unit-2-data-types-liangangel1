162d54d9e81fc8be30be00c7cdcc40e200da9cfe
liangangel1 <angell213@nycstudents.net>
2024-02-06 13:57:59 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,17 @@
 
-letters =["a", "b", "c", "d", "e"]
-print(letters)
-for alphabet in letters:
-    print (letters)
+bill = int(input ("money spent on food"))
+
+tip = int(input("how much tips"))
+
+tip = 5
+if tip > 1:
+    print('tip recommended')
+elif tip == 5:
+    print('perfect')
+else:
+    print('come back soon')
+
+
+print(bill+tip)
+
 
