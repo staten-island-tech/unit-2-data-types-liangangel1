3c7fa236e3ee3b3d9dc76bd9c2166c4deeb9c41f
liangangel1 <angell213@nycstudents.net>
2024-02-07 13:57:07 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,9 +1,32 @@
+# calc
+#bill = int(input ("money spent on food"))
+#tip = int(input("how much tips"))
+
+#tip = 5
+#if tip > 1:
+#    print('tip recommended')
+#elif tip == 5:
+#    print('perfect')
+#else:
+#    print('come back soon')
+
+#print(bill+tip)
+
+#---------------------------------------------------------------
+#x = (int(input ("number")))
+
+#number = x
+#if number%2 == 1:
+#    print ("odd")
+#else:
+#    print('even')
+
+#---------------------------------------------------------------
+
 
 bill = int(input ("money spent on food"))
 
-tip = int(input("how much tips"))
-
-tip = 5
+tip = 
 if tip > 1:
     print('tip recommended')
 elif tip == 5:
@@ -11,7 +34,4 @@ elif tip == 5:
 else:
     print('come back soon')
 
-
 print(bill+tip)
-
-
